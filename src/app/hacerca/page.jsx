c9206fe670ae642bc3aadabdab0b02c019fb977e
waltercardona <wacardona10@gmail.com>
2024-09-11 11:39:07 -0500

    iniciando proyecto para prueba tecnica

---
+++ src/app/hacerca/page.jsx
@@ -0,0 +1,4 @@
+  function Hacerca() {
+    return <h1>estoy en la pagina de hacerca???</h1>
+}
+export default Hacerca
---
+++ src/app/page.jsx
@@ -0,0 +1,3 @@
+export default function HomePage(){
+    return <h1>hola desde mi primera pagina en next</h1>
+}
